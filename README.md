b5e090ed17e696cd0d14d64d09de1651e79027e0
groubaud101 <guillaume.roubaud1@gmail.com>
2021-03-20 17:20:58 +0100

    ajout de fonction à la lib

--- README.md
+++
@@ -1,6 +0,0 @@
-# ft_printf
-
-ft_printf project from 19 Bruxelle (42 school)
-
-man 3 printf
-man 3 stdarg
--- libft/libft.h
+++ libft/libft.h
@@ -57,7 +57,7 @@ int		ft_toupper(int c);
 ** Looking for char in str
 */
 
-int		ft_strcmp(const char *s1, const char *s2); // a faire
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
@@ -73,8 +73,8 @@ char	*ft_strncat(char *dst, const char *src, size_t size); // a faire
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
-char	*ft_strnstr(const char *haystack, const char *needle); // a faire
-char	*ft_strstr(const char *haystack, const char *needle, size_t n);
+char	*ft_strstr(const char *haystack, const char *needle); // a faire
+char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
 size_t	ft_strlen(const char *s);
 
 /*
