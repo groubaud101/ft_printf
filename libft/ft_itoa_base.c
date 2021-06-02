76ff37849ebcfbfbc27a2c1dec1ec639a0541f69
groubaud101 <guillaume.roubaud1@gmail.com>
2021-06-02 21:50:37 +0200

    itoa_base, ne foncionnait que pour une base 10, un peu limité

--- libft/ft_itoa_base.c
+++ libft/ft_itoa_base.c
@@ -12,11 +12,11 @@
 
 #include "libft.h"
 
-static char	*ft_rec_itoa_base(int n, char *nbr, int i, char *base)
+static char	*ft_rec_itoa_base(int n, char *nbr, int i, char *base, int len_base)
 {
-	if (n > 9 || n < -9)
-		nbr = ft_rec_itoa_base(n / ft_strlen(base), nbr, i - 1, base);
-	nbr[i] = base[ft_abs(n % ft_strlen(base))];
+	if (n >= len_base || n <= -len_base)
+		nbr = ft_rec_itoa_base(n / len_base, nbr, i - 1, base, len_base);
+	nbr[i] = base[ft_abs(n % len_base)];
 	return (nbr);
 }
 
@@ -24,12 +24,14 @@ char	*ft_itoa_base(int n, char *base)
 {
 	int		len_nbr;
 	char	*nbr;
+	int		len_base;
 
-	len_nbr = ft_len_int_base(n, ft_strlen(base));
+	len_base = ft_strlen(base);
+	len_nbr = ft_len_int_base(n, len_base);
 	nbr = (char *)ft_calloc(sizeof(*nbr), len_nbr + 1);
 	if (!nbr)
 		return (NULL);
-	nbr = ft_rec_itoa_base(n, nbr, len_nbr - 1, base);
+	nbr = ft_rec_itoa_base(n, nbr, len_nbr - 1, base, len_base);
 	if (n < 0)
 		nbr[0] = '-';
 	return (nbr);
